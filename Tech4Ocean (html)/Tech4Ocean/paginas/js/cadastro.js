b1a940849984eeeea82600a9c0a675f308e53ef4
Pedro-sugoy <pedrovisckyokoo@gmail.com>
2024-06-06 12:09:09 -0300

    nova pagina

---
+++ Tech4Ocean (html)/Tech4Ocean/paginas/js/cadastro.js
@@ -0,0 +1,9 @@
+let enviar = document.querySelector('#form')
+
+enviar.addEventListener('submit',()=>{
+    alert("Obrigado por se cadastrar")
+})
+
+enviar.addEventListener('reset',()=>{
+    alert("Limpando os dados colocados!")
+})
